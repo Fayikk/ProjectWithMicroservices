a44f143455665ad30480e18319c58a0980d09fcc
Fayik <veznedaroglufayik2@gmail.com>
2022-11-13 13:39:08 +0300

    Added Basket

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Threading;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+      public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>(); 
+    
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
+
+
+
