f10f8c60c103695620df0c16ed88a60ca2832d45
Fayik <veznedaroglufayik2@gmail.com>
2022-11-16 21:06:02 +0300

    Update

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteCommand/DeleteOrderCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteCommand
+{
+    public class DeleteOrderCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
